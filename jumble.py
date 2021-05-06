407423f8ac920156e4f5cb0793291a454024711a
rohan37-pro <75291986+rohan37-pro@users.noreply.github.com>
2021-05-06 04:15:33 -0700

    file uploaded

---
+++ jumble.py
@@ -0,0 +1,24 @@
+import itertools
+
+while True:
+	jumble = input("enter a jubmle word : ")
+
+	if jumble == "exit" or jumble=="quit":
+		break
+
+	lis = []
+	a = itertools.permutations(jumble)
+	for i in a :
+		i = "".join(i)
+		lis.append(i)
+
+	with open("C:\\Users\\rohan\\Desktop\\codes\\txt\\dictionary_words.txt",'r') as file:
+		words = file.readlines()
+
+		words_lis = []
+		for i in words:
+			words_lis.append(i.strip())
+
+		for i in words_lis:
+			if i.lower() in lis:
+				print(i)
