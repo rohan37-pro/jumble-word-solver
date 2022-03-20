8dbc97255d3d5ab62b07a21a90c46cd85b285af6
rohan37-pro <rohan2002maji@gmail.com>
2022-03-20 10:33:33 -0400

    word search speed increased

--- jumble.py
+++ jumble.py
@@ -1,5 +1,33 @@
 import itertools
-import os 
+import os
+import time
+import string
+
+
+
+with open(f"{os.getcwd()}/dictionary2.0.txt",'r') as file:
+		words = file.readlines()
+
+		words_lis = []
+		for i in words:
+			words_lis.append(i.lower().strip())
+
+		alpha_range = {}
+		found_char = 0
+		alphabates = string.ascii_lowercase
+		for i in alphabates:
+			for j in words_lis:
+				if j.startswith(f"{i}") and found_char == 0:
+					alpha_range[i] = [words_lis.index(j)]
+					found_char = 1
+				elif not j.startswith(f'{i}') and found_char == 1:
+					alpha_range[i].append(words_lis.index(j))
+					found_char = 0
+
+		alpha_range[i].append(None)
+		#print(alpha_range)
+
+
 
 while True:
 	jumble = input("enter a jubmle word : ")
@@ -13,13 +41,13 @@ while True:
 		i = "".join(i)
 		lis.append(i)
 
-	with open(f"{os.getcwd()}/dictionary_words.txt",'r') as file:
-		words = file.readlines()
 
-		words_lis = []
-		for i in words:
-			words_lis.append(i.strip())
+	start = time.time()
+	for word in lis:
+		if word in words_lis[alpha_range[word[0]][0]:alpha_range[word[0]][1]]:
+			print(f"{word} --> query ends in {time.time() - start} secs...")
 
-		for i in words_lis:
-			if i.lower() in lis:
-				print(i)
+
+
+	
+
